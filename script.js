c9ce96be28c71a62a4b3a850495205df4bf3e32b
nobel7761 <habiburnobel@gmail.com>
2023-02-24 02:33:47 +0600

    assignment completed

--- script.js
+++ script.js
@@ -1,104 +1,188 @@
-const containerEL = document.getElementById("all-matches container");
-const containerAddBtn = document.getElementById("nobel-add-new-container");
+const containerEL = document.getElementById("container");
+const addMatchButton = document.getElementById("add-match");
+const resetButton = document.getElementById("reset");
 
-let componentId = 2;
-
-const addNewComponent = () => {
-  const rootDiv = document.createElement("div");
-  rootDiv.classList.add("match");
-
-  const firstChildDiv = document.createElement("div");
-  firstChildDiv.classList.add("wrapper");
-  rootDiv.appendChild(firstChildDiv);
-
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("lws-delete");
-  firstChildDiv.appendChild(deleteButton);
-
-  const deleteImage = document.createElement("img");
-  deleteImage.setAttribute("src", "./image/delete.svg");
-  deleteButton.appendChild(deleteImage);
-
-  deleteButton.addEventListener("click", () => {
-    containerEL.removeChild(rootDiv);
-  });
-
-  const matchTitle = document.createElement("h3");
-  matchTitle.classList.add("lws-matchName");
-  matchTitle.textContent = `Match ${componentId}`;
-  firstChildDiv.appendChild(matchTitle);
-
-  const secondChildDiv = document.createElement("div");
-  secondChildDiv.classList.add("inc-dec");
-  rootDiv.appendChild(secondChildDiv);
-
-  const incrementForm = document.createElement("form");
-  incrementForm.classList.add("incrementForm");
-  secondChildDiv.appendChild(incrementForm);
-
-  const incrementTitle = document.createElement("h4");
-  incrementTitle.textContent = "Increment";
-  incrementForm.appendChild(incrementTitle);
-
-  const incrementInput = document.createElement("input");
-  incrementInput.setAttribute("type", "number");
-  incrementInput.setAttribute("name", "increment");
-  incrementInput.classList.add("lws-increment");
-  incrementForm.appendChild(incrementInput);
-
-  const decrementForm = document.createElement("form");
-  decrementForm.classList.add("decrementForm");
-  secondChildDiv.appendChild(decrementForm);
-
-  const decrementTitle = document.createElement("h4");
-  decrementTitle.textContent = "Decrement";
-  decrementForm.appendChild(decrementTitle);
-
-  const decrementInput = document.createElement("input");
-  decrementInput.setAttribute("type", "number");
-  decrementInput.setAttribute("name", "decrement");
-  decrementInput.classList.add("lws-decrement");
-  decrementForm.appendChild(decrementInput);
-
-  const thirdChildDiv = document.createElement("div");
-  thirdChildDiv.classList.add("numbers");
-  rootDiv.appendChild(thirdChildDiv);
-
-  const resultEL = document.createElement("h2");
-  resultEL.classList.add("lws-singleResult");
-  resultEL.textContent = "120";
-  thirdChildDiv.appendChild(resultEL);
-
-  incrementInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      const incrementValue = Number(e.target.value);
-      resultEL.textContent = String(
-        Number(resultEL.textContent) + incrementValue
-      );
-      e.preventDefault();
-    }
-  });
-
-  decrementInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      const decrementValue = Number(e.target.value);
-      const updatedValue = String(
-        Number(resultEL.textContent) - decrementValue
-      );
-      if (updatedValue >= 0) {
-        resultEL.textContent = updatedValue;
-      } else {
-        resultEL.textContent = 0;
-      }
-      e.preventDefault();
-    }
-  });
-
-  // Add the new component to the container
-  containerEL.appendChild(rootDiv);
-
-  // Increment the component ID for the next component
-  componentId++;
+// GET MATCH
+const getMatch = (id, matchNo) => {
+  return `<div id="match-${id}" class="match">
+        <div class="wrapper">
+          <button id="delete-${id}" class="lws-delete">
+            <img src="./image/delete.svg" alt="" />
+          </button>
+          <h3 class="lws-matchName">Match ${matchNo}</h3>
+        </div>
+        <div class="inc-dec">
+          <form class="incrementForm">
+            <h4>Increment</h4>
+            <input
+              id="increment-${id}"
+              type="number"
+              name="increment"
+              class="lws-increment"
+            />
+          </form>
+          <form class="decrementForm">
+            <h4>Decrement</h4>
+            <input
+              id="decrement-${id}"
+              type="number"
+              name="decrement"
+              class="lws-decrement"
+            />
+          </form>
+        </div>
+        <div class="numbers">
+          <h2 id="score-${id}" class="lws-singleResult"></h2>
+        </div>
+      </div>`;
 };
 
-containerAddBtn.addEventListener("click", addNewComponent);
+// ACTION IDENTIFIERS
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+const ADD_MATCH = "addNewMatch";
+const RESET_SCORE = "resetScore";
+const DELETE_MATCH = "deleteMatch";
+
+// ACTION CREATORS
+const increment = (score, id) => {
+  return {
+    type: INCREMENT,
+    payload: { score: score, id: id },
+  };
+};
+const decrement = (score, id) => {
+  return {
+    type: DECREMENT,
+    payload: { score: score, id: id },
+  };
+};
+const addMatch = () => {
+  return {
+    type: ADD_MATCH,
+  };
+};
+const resetScores = () => {
+  return {
+    type: RESET_SCORE,
+  };
+};
+
+// INITIAL STATE
+const initialState = {
+  matches: [
+    {
+      id: 1,
+      score: 0,
+    },
+  ],
+};
+
+//REDUCER FUNCTION
+const scoreReducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      ...state,
+      matches: state.matches.map((match) =>
+        match.id === action.payload.id
+          ? { ...match, score: match.score + action.payload.score }
+          : { ...match }
+      ),
+    };
+  } else if (action.type === DECREMENT) {
+    return {
+      ...state,
+      matches: state.matches.map((match) =>
+        match.id === action.payload.id
+          ? { ...match, score: match.score - action.payload.score }
+          : { ...match }
+      ),
+    };
+  } else if (action.type === ADD_MATCH) {
+    return {
+      ...state,
+      matches: [
+        ...state.matches,
+        {
+          id: state.matches.length + 1,
+          score: 0,
+        },
+      ],
+    };
+  } else if (action.type === RESET_SCORE) {
+    return {
+      ...state,
+      matches: state.matches.map((match) => ({
+        ...match,
+        score: 0,
+      })),
+    };
+  } else {
+    return { ...state };
+  }
+};
+
+// CREATE STORE
+const store = Redux.createStore(scoreReducer);
+
+const render = () => {
+  const state = store.getState();
+  console.log("state", state);
+  state.matches.forEach((match, i) => {
+    if (!document.getElementById(`match-div-${i}`)) {
+      const matchDiv = document.createElement("div");
+      matchDiv.setAttribute("id", `match-div-${i}`);
+      matchDiv.setAttribute("class", `match-div`);
+      matchDiv.innerHTML = getMatch(match.id, i + 1);
+
+      containerEL.append(matchDiv);
+
+      const incrementInput = document.getElementById(`increment-${match.id}`);
+      const decrementInput = document.getElementById(`decrement-${match.id}`);
+
+      incrementInput.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          const score = parseInt(incrementInput.value) || 0;
+          store.dispatch(increment(score, match.id));
+          incrementInput.value = "";
+        }
+      });
+
+      decrementInput.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          const score = parseInt(decrementInput.value) || 0;
+          const currentScore = store
+            .getState()
+            .matches.find((m) => m.id === match.id).score;
+
+          if (score >= currentScore) {
+            store.dispatch(decrement(currentScore, match.id));
+            decrementInput.value = "";
+          } else {
+            store.dispatch(decrement(score, match.id));
+            decrementInput.value = "";
+          }
+
+          console.log(score, currentScore);
+        }
+      });
+    }
+    const score = document.getElementById(`score-${match.id}`);
+    score.innerHTML = match.score;
+  });
+};
+
+//UPDATE UI MANUALLY
+render();
+
+addMatchButton.addEventListener("click", () => {
+  store.dispatch(addMatch());
+});
+
+resetButton.addEventListener("click", () => {
+  store.dispatch(resetScores());
+});
+
+store.subscribe(render);
